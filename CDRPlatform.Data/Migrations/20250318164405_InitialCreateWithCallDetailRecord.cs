ed38cde3b1071a593492b22865ab6111bf40f6c3
Bohdan.Volkov <Bohdan.Volkov@giacom.com>
2025-03-18 20:30:58 +0100

    Initial db migration

---
+++ CDRPlatform.Data/Migrations/20250318164405_InitialCreateWithCallDetailRecord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CDRPlatform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateWithCallDetailRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CallDetailRecord",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CallerId = table.Column<long>(type: "bigint", nullable: true),
+                    Recipient = table.Column<long>(type: "bigint", nullable: true),
+                    CallDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(20,10)", precision: 20, scale: 10, nullable: false),
+                    Reference = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CallDetailRecord", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CallDetailRecord");
+        }
+    }
+}
